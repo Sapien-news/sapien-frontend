2bc5beda85be4f895b65997a0a53461722dd6fc2
Jithin KS <jithinks@Jithins-MacBook-Pro.local>
2022-05-18 22:39:32 +0530

    card component

--- components/home/ReadCard.tsx
+++ components/home/ReadCard.tsx
@@ -1,7 +1,26 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
+import { Box, Heading } from "@chakra-ui/react";
 
-const ReadCard = () => {
-  return <div>ReadCard</div>;
+const ReadCard = (props: any) => {
+  const { heading, content, author } = props;
+
+  return (
+    <Box p="5" maxW="300" bg="black">
+      <img alt="" src="card-image.png"></img>
+      <Box>
+        <Heading mt="3" size="lg" color="white">
+          {heading}
+        </Heading>
+        <Box mt="3" fontSize="15" color="white">
+          {content}
+        </Box>
+        <Box color="white" fontWeight={"bold"} mt="3">
+          Author: {author}
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default ReadCard;
