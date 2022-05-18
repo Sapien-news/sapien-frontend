5b916b371408c6b04bb32b624343791a0a7c99c9
Sanjay <sanjayrjs16@gmail.com>
2022-05-18 22:52:54 +0530

    Colors added to theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import React, { FC, useMemo } from "react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import "@fontsource/space-grotesk/300.css";
@@ -14,7 +15,36 @@ import {
   WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
 
+import {
+  GlowWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  SolletExtensionWalletAdapter,
+  SolletWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+
+import { clusterApiUrl } from "@solana/web3.js";
+
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const network = WalletAdapterNetwork.Devnet;
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new GlowWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+    ],
+    [network]
+  );
+
   return (
     <ChakraProvider theme={theme}>
       <ConnectionProvider endpoint={endpoint}>
--- styles/theme.ts
+++ styles/theme.ts
@@ -3,6 +3,14 @@ import { extendTheme } from "@chakra-ui/react";
 // 2. Extend the theme to include custom colors, fonts, etc
 const colors = {
   brand: {
+    greyDark: "#1E1E1E",
+    greyBorder: "#555555",
+    greyText: "#696969",
+    primaryBG: "#5B5B5B",
+    primaryRed: "#FF0000",
+    purpleLight: "#DED9FF",
+    purpleDark: "#4C4096",
+    white: "#FFFFFF",
     900: "#1a365d",
     800: "#153e75",
     700: "#2a69ac",
